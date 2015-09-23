f9c096298d35d1c1015bea8397126772f1ec2953
toanhtran <toanh_tran@yahoo.com>
2015-09-23 22:08:24 +0900

    update button and eatCookie method

--- app/src/main/java/com/example/toanhtran/cookies/MainActivity.java
+++ app/src/main/java/com/example/toanhtran/cookies/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.cookies;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.toanhtran.cookies.R;
 
@@ -19,9 +21,12 @@ public class MainActivity extends AppCompatActivity {
      * Called when the cookie should be eaten.
      */
     public void eatCookie(View view) {
-        // TODO: Find a reference to the ImageView in the layout. Change the image.
+        // Find a reference to the ImageView in the layout. Change the image.
+        ImageView imageView = (ImageView) findViewById(R.id.android_cookie_image_view);
+        imageView.setImageResource(R.drawable.after_cookie);
 
-        // TODO: Find a reference to the TextView in the layout. Change the text.
-
+        // Find a reference to the TextView in the layout. Change the text.
+        TextView textView = (TextView) findViewById(R.id.status_text_view);
+        textView.setText("I'm so full");
     }
 }
